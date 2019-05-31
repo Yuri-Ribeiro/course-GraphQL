b74e5f96835ab72107121536c91e4aa8f767cfb8
Guest User <Guest@Leonardos-MacBook-Pro.local>
2019-05-31 11:59:56 -0300

    Created mutation for new user

--- mutation/data/db.js
+++ mutation/data/db.js
@@ -1,24 +1,29 @@
+let id = 1
+function proximoID(){
+    return id++
+}
+
 const perfis = [
     { id: 1, nome: 'comum' },
     { id: 2, nome: 'administrador' }
 ]
 
 const usuarios = [{
-    id: 1,
+    id: proximoID(),
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
     perfil_id: 1,
     status: 'ATIVO'
 }, {
-    id: 2,
+    id: proximoID(),
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
     perfil_id: 2,
     status: 'INATIVO'
 }, {
-    id: 3,
+    id: proximoID(),
     nome: 'Daniela Smith',
     email: 'danismi@umail.com',
     idade: 24,
@@ -26,4 +31,4 @@ const usuarios = [{
     status: 'BLOQUEADO'
 }]
 
-module.exports = { usuarios, perfis }
+module.exports = { usuarios, perfis, proximoID }
---
+++ mutation/resolvers/Mutation.js
@@ -0,0 +1,17 @@
+const { usuarios, proximoID } = require('../data/db')
+
+module.exports = {
+    novoUsuario(_, { nome, email, idade }){
+        const novo = {
+            id: proximoID(),
+            nome,
+            email,
+            idade,
+            perfil_id: 1,
+            status: 'ATIVO'
+        }
+
+        usuarios.push(novo)
+        return novo
+    }
+}
