7f6fdb32f69fa9d0dbb4d87082ae8d20eaa9cd21
Yuri Barros <barrosribeiroyuri@gmail.com>
2019-06-02 01:32:34 -0300

    Using input to filter

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,5 +1,15 @@
 const { usuarios, proximoID } = require('../data/db')
 
+function indiceUsuario(filtro){
+    if(!filtro) return -1
+    const { id, email } = filtro
+    if(filtro.id)
+        return usuarios.findIndex( usuario => usuario.id === id)
+    else if(email)
+        return usuarios.findIndex( usuario => usuario.email === email)
+    return -1
+}
+
 module.exports = {
     novoUsuario(_, { dados }){
         const emailExistente = usuarios
@@ -17,8 +27,9 @@ module.exports = {
         usuarios.push(novo)
         return novo
     },
-    excluirUsuario(_, { id }){
-        const i = usuarios.findIndex( usuario => usuario.id === id)
+    excluirUsuario(_, { filtro }){
+        const i = indiceUsuario(filtro)
+        
         if( i < 0 ) return null
         const excluidos =
             usuarios.splice(i, 1)
