6ad916941f6059e810733880faded5289b4ff6c1
Yuri Barros <barrosribeiroyuri@gmail.com>
2019-06-02 00:00:28 -0300

    Mutation to update user

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -21,5 +21,25 @@ module.exports = {
             usuarios.splice(i, 1)
         return excluidos ?
             excluidos[0]: null
+    },
+    alterarUsuario(_, args){
+        const i = usuarios.findIndex( usuario => usuario.id === args.id)
+        if( i < 0 ) return null
+        if(args.nome){
+            usuarios[i].nome = args.nome
+        }
+        if(args.email){
+            usuarios[i].email = args.email
+        }
+        if(args.idade){
+            usuarios[i].idade = args.idade
+        }
+        return usuarios[i]
+        // const usuario = {
+        //     ...usuarios[i],
+        //     ...args
+        // }
+        // usuarios.splice(i, 1, usuario)
+        // return usuario
     }
 }
