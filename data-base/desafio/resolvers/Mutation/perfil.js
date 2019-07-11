bb2dd62a685b1b742d07733576609518fec676c6
yuri-ribeiro <barrosribeiroyuri@gmail.com>
2019-07-11 13:28:16 -0300

    Challenge finalized

--- data-base/desafio/resolvers/Mutation/perfil.js
+++ data-base/desafio/resolvers/Mutation/perfil.js
@@ -14,9 +14,26 @@ module.exports = {
         return perfil
     },
     async excluirPerfil(_, { filtro }) {
-        // implementar
+        let perfil = filtro.id ? await db('perfis').where({id: filtro.id}).first(): await db('perfis').where({nome: filtro.nome}).first()
+
+        if(perfil && !await db('usuarios_perfis').where({perfil_id: perfil.id}).first()){
+            await db('perfis').where({nome: filtro.nome}).delete()
+            return perfil
+        }
     },
     async alterarPerfil(_, { filtro, dados }) {
-        // implementar
+        let perfil = filtro.id ? await db('perfis').where({id: filtro.id}).first(): await db('perfis').where({nome: filtro.nome}).first()
+        
+        if(await db('perfis').where({nome: dados.nome}).first())
+            return null
+
+        if(!perfil){
+            const id = await db('perfis').insert(dados)
+            perfil = await db('perfis').where({id}).first()
+        } else {
+            await db('perfis').where({id: perfil.id}).update(dados)
+            perfil = {...perfil, ...dados}
+        }
+        return perfil
     }
 }
