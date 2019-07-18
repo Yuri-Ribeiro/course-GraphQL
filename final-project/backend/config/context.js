6202ff8f44b2699816975c509a5dc0ca7ec73ea9
Yuri Ribeiro <barrosribeiroyuri@gmail.com>
2019-07-18 13:00:23 -0300

    Using context #2

--- final-project/backend/config/context.js
+++ final-project/backend/config/context.js
@@ -1,7 +1,28 @@
+const jwt = require('jwt-simple')
+
 module.exports = async ({ req }) => {
     // Em desenvolvimento
     await require('./simularUsuarioLogado')(req)
 
     const auth = req.headers.authorization
-    console.log(auth)
+    const token = auth && auth.substring(7)
+
+    let usuario = null
+    let admin = false
+
+    if(token) {
+        try {
+            let conteudoToken = jwt.decode(token,
+                process.env.APP_AUTH_SECRET)
+            if(new Date(conteudoToken.exp * 1000) > new Date()) {
+                usuario = conteudoToken
+            }
+        } catch(e) {
+            // Invalid token
+        }
+    }
+
+    if(usuario && usuario.perfis) {
+        admin = usuario.perfis.includes('admin')
+    }
 }
